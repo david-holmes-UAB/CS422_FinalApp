a3a8dba86769f908c41415db8606f2226658ce19
Ruler (David) <azurecode516@gmail.com>
2023-04-06 05:23:08 -0500

    data class skeletons from Damon
    see title

--- MusicTracker/app/src/main/java/cs/mad/musictracker/bases/spotifyUser.kt
+++ MusicTracker/app/src/main/java/cs/mad/musictracker/bases/spotifyUser.kt
@@ -10,6 +10,30 @@ data class spotifyUser (
     val country: String?
     )
 
+// Object data classes; structure from Damon
+data class Artist (
+    val id: String,
+    val name: String
+)
+data class Album (
+    val id: String,
+    val name: String,
+    val songs: List<Song>
+        )
+data class Song (
+    val intID: String,
+    val title: String,
+    val length: Int,
+    val artist: Artist,
+    val album: Album
+        )
+
+data class listenData (
+    val yrHrs: Int,
+    val topSongs: List<Song>,
+    val topArtists: List<Artist>
+        )
+
 @Dao
 interface userDao {
     // TODO
