4bbd230aa10552f776bd2562ef1879ed7629a70e
Damon Lee <leedamon82@yahoo.com>
2023-04-07 17:23:57 -0500

    Messing around with AuthFlow

--- MusicTracker/app/src/main/java/cs/mad/musictracker/MainActivity.kt
+++ MusicTracker/app/src/main/java/cs/mad/musictracker/MainActivity.kt
@@ -11,13 +11,32 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import cs.mad.musictracker.databinding.ActivityMainBinding
 
+import com.spotify.android.appremote.api.ConnectionParams
+import com.spotify.android.appremote.api.Connector;
+import com.spotify.android.appremote.api.SpotifyAppRemote;
+
+import com.spotify.protocol.client.Subscription;
+import com.spotify.protocol.types.PlayerState;
+import com.spotify.protocol.types.Track;
+
+import android.util.Log;
+
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val clientId = "ad0911afa57949bba362003f601876b2"
+    private val redirectUri = "https://com.spotify.android.spotifysdkkotlindemo/callback"
+    //private val clientId = "671cc4720b4f420397ecd146b7eb09b1"
+    //private val redirectUri = "https://example.com/callback"
+    private var spotifyAppRemote: SpotifyAppRemote? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+        /*
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -33,18 +52,54 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+         */
     }
 
     // Below functions are first provided steps from Spotify
     override fun onStart() {
         super.onStart()
+        val connectionParams = ConnectionParams.Builder(clientId)
+            .setRedirectUri(redirectUri)
+            .showAuthView(true)
+            .build()
+
+        SpotifyAppRemote.connect(this, connectionParams, object : Connector.ConnectionListener {
+            override fun onConnected(appRemote: SpotifyAppRemote) {
+                spotifyAppRemote = appRemote
+                Log.d("MainActivity", "Connected! Yay!")
+                // Now you can start interacting with App Remote
+                connected()
+            }
+
+            override fun onFailure(throwable: Throwable) {
+                Log.e("MainActivity", throwable.message, throwable)
+                // Something went wrong when attempting to connect! Handle errors here
+            }
+        })
     }
+
 
     private fun connected() {
 
+        spotifyAppRemote?.let {
+            // Play a playlist
+            val playlistURI = "spotify:playlist:37i9dQZF1DX2sUQwD7tbmL"
+            it.playerApi.play(playlistURI)
+            // Subscribe to PlayerState
+            it.playerApi.subscribeToPlayerState().setEventCallback {
+                val track: Track = it.track
+                Log.d("MainActivity", track.name + " by " + track.artist.name)
+            }
+        }
+
     }
+
 
     override fun onStop() {
         super.onStop()
+        spotifyAppRemote?.let {
+            SpotifyAppRemote.disconnect(it)
+        }
     }
 }
--- MusicTracker/app/src/main/java/cs/mad/musictracker/bases/UserDatabase.kt
+++ MusicTracker/app/src/main/java/cs/mad/musictracker/bases/UserDatabase.kt
@@ -3,5 +3,7 @@ package cs.mad.musictracker.bases
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
+// test test
 class UserDatabase {
+
 }
